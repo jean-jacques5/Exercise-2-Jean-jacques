88d1fab88762a18b2d37c8e42e2e549f57a83256
jean-jacques5 <146061464+jean-jacques5@users.noreply.github.com>
2025-10-15 16:18:20 +0200

    correction du fichier app.js

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(express.json());        // parser JSON
 app.use("/", taskRoutes);       // routes MVC
 
 app.listen(PORT, () => {
-  console.log(`✅ ToDoList API (Express) ready on http://127.0.0.1:${PORT}`);
+  console.log(`✅ ToDoList API (Express) pret a cette url http://127.0.0.1:${PORT}`);
 });
 module.exports = app;
